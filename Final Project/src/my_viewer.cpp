91d53e370827bd3ef6c31596cfa06f7aa8426f01
sunsunjackson <jackosnzh@gmail.com>
2019-12-17 23:07:28 -0800

    Update my_viewer.cpp

--- Final Project/src/my_viewer.cpp
+++ Final Project/src/my_viewer.cpp
@@ -1,27 +1,28 @@
 
-// by Ritesh Sharma and Marcelo Kallmann
-
 # include "my_viewer.h"
 
 # include <sigogl/ui_button.h>
 # include <sigogl/ui_radio_button.h>
 # include <sig/sn_primitive.h>
-# include <sig/sn_material.h>
 # include <sig/sn_transform.h>
 # include <sig/sn_manipulator.h>
 
 # include <sigogl/ws_run.h>
 
+SnGroup* gCity;
+SnGroup* gFalcon;
+SnGroup* g;
+SnTransform* tCity;
+SnTransform* tFalcon;
 float gtime;
-bool camView = true;
 
 MyViewer::MyViewer ( int x, int y, int w, int h, const char* l ) : WsViewer(x,y,w,h,l)
 {
 	_nbut=0;
 	_animating=false;
+	background(GsColor::black);
 	build_ui ();
 	build_scene ();
-	update_shadow();
 }
 
 void MyViewer::build_ui ()
@@ -37,190 +38,198 @@ void MyViewer::build_ui ()
 	p->add ( new UiButton ( "Exit", EvExit ) ); p->top()->separate();
 }
 
-void MyViewer::add_model ( SnGroup* parentg, SnShape* s, GsVec p )
+void MyViewer::add_model ( SnShape* s, GsVec p )
 {
-	SnGroup* g = new SnGroup; // group will have: transf, shape, lines
-	SnTransform* t = new SnTransform;
+	// This method demonstrates how to add some elements to our scene graph: lines,
+	// and a shape, and all in a group under a SnManipulator.
+	// Therefore we are also demonstrating the use of a manipulator to allow the user to
+	// change the position of the object with the mouse. If you do not need mouse interaction,
+	// you can just use a SnTransform to apply a transformation instead of a SnManipulator.
+	// You would then add the transform as 1st element of the group, and set g->separator(true).
+	// Your scene graph should always be carefully designed according to your application needs.
+
+	SnManipulator* manip = new SnManipulator;
+	GsMat m;
+	m.translation ( p );
+	manip->initial_mat ( m );
+
+	SnGroup* g = new SnGroup;
 	SnLines* l = new SnLines;
 	l->color(GsColor::orange);
-	g->add(t);
 	g->add(s);
 	g->add(l);
-	g->separator(true);
-	parentg->add(g);
+	manip->child(g);
+	// manip->visible(false); // call this to turn off mouse interaction
+
+	rootg()->add(manip);
+}
+
+static int factorial(int n) {
+	if (n == 0) 
+		return 1;
+	else return (n * factorial(n - 1));
+}
+
+static GsPnt bezier(float t, GsArray<GsPnt>& P) {
+	GsPnt point;
+	int n = P.size() - 1;
+	for (int i = 0; i < P.size(); i++) {
+		int C = factorial(n) / (factorial(i) * factorial(n - i));
+		point += P[i] * float(C) * powf((1.0f - t), float(n - i)) * powf(t, (float)i);
+	}
+	return point;
+}
+
+static GsPnt CRSplines(float t, int i, GsArray<GsPnt>& P) {
+	GsPnt I1 = (P[i + 1] - P[i - 1]) / 2;
+	GsPnt I2 = (P[i + 2] - P[i]) / 2;
+	GsPnt Pp = P[i] + (I1 / 3);
+	GsPnt Pm = P[i + 1] + (I2 / 3);
+	
+	GsArray<GsPnt> temp; 
+	temp.push() = P[i];
+	temp.push() = Pp;
+	temp.push() = Pm;
+	temp.push() = P[i + 1];
+
+	return bezier(t, P);
 }
 
 void MyViewer::build_scene ()
 {
-	SnPrimitive* p;
-	_gHands = new SnGroup;
 
-	//// Minutes Hand
-	//p = new SnPrimitive;
-	//p->prim().cylinder( GsVec(0,0,0), GsVec(0,mh_Length,0), mh_Length/20.0f );
-	////p->prim().box(2, 2.5f, 2);
-	//p->prim().material.diffuse=GsColor::blue;
-	//add_model(_gHands, p, GsVec(0, sh_Length, 0));
+	GsMat trans;
 
-	//// Seconds Hand
-	//p = new SnPrimitive;
-	//p->prim().cylinder ( GsVec(0,0,0), GsVec(0,sh_Length,0), sh_Length/24.0f );
-	//p->prim().material.diffuse = GsColor::red;
-	//add_model(_gHands, p, GsVec(0, sh_Length, 0));
+	gCity = new SnGroup;
+	gFalcon = new SnGroup;
+	g = new SnGroup;
 
-	// Dial
-	float R = dialR;
-	/*p = new SnPrimitive(GsPrimitive::Box, R, R, R/10.0f);*/
-	p = new SnPrimitive(GsPrimitive::Box, R/2.0f, R, R/2.0f);
-	//p->prim().material.diffuse = GsColor(0.0f,1.0f,0.0f,0.1f);
-	p->prim().material.diffuse = GsColor::red;
-	//p->prim().orientation = GsQuat(GsVec::i, (float)GS_PI / 2);
-	SnGroup* gdial = new SnGroup;
-	add_model(gdial, p, GsVec(0, 0, 0));
+	SnModel* mCity = new SnModel;
+	//rpX = 5.0f; rpY = -4.5f; rpZ = -3.5f;
+	if (!mCity->model()->load("C:/Users/Lopez/OneDrive/Desktop/CSE/CSE170/FinalProject/sigapp/vs2019/city/sirus City.obj")) {
+		gsout << "ha sucker" << gsnl;
+		
+	}
+	mCity->model()->scale(0.5f);
+	//rightPeddle->model()->centralize();
+	gCity->separator(true);
+	gCity->add(tCity = new SnTransform);
+	gCity->add(mCity);
+	//trans.translation(GsVec(0, 0.0f, 0));
+	//tCity->set(trans);	
 
-	// Shadow material
-	SnMaterial* snm = new SnMaterial;
-	GsMaterial mtl;
-	mtl.diffuse = GsColor::black; // shadow color
-	//snm->material ( mtl, 2 ); // will override the used color for 2 shapes that come next
-	//snm->restore ( true );
+	SnModel* mFalcon = new SnModel;
+	gsout << "check" << gsnl;
+	//rpX = 5.0f; rpY = -4.5f; rpZ = -3.5f;
+	if (!mFalcon->model()->load("C:/Users/Lopez/OneDrive/Desktop/CSE/CSE170/FinalProject/sigapp/vs2019/falcon/millenium-falcon.obj")) {
+		gsout << "ha sucker" << gsnl;
+	}
+	gsout << "check 2" << gsnl;
+	mFalcon->model()->scale(0.10f);
+	//mFalcon->model()->centralize();
+	gFalcon->separator(true);
+	gFalcon->add(tFalcon = new SnTransform);
+	
+	gFalcon->add(mFalcon);
+	trans.translation(GsVec(0.0f, 250.0f, 0.0f));
+	tFalcon->set(trans);
 
-	// Light pos:
-	SnPrimitive* s = new SnPrimitive;
-	s->color(GsColor::yellow);
-	s->prim().sphere(0.2f);
-	_gLight = new SnGroup ( new SnTransform, s, true );
-	_gLight->get<SnTransform>(0)->get().setrans(lightPos);
+	gsout << "hello world";
 
-	// Build Scene graph sharing the clock group:
-	rootg()->add (_gLight);
-	rootg()->add (_gHands);
-	rootg()->add (gdial);
+	g->add(gFalcon);
+	g->add(gCity);
+	rootg()->add(g);
 
-	SnGroup* dshadowg = new SnGroup; // we use 2 shadow planes (almost the same) to avoid dial and hands overlap
-	dshadowg->separator(true);
-	dshadowg->add (_tShadow1 = new SnTransform);
-	dshadowg->add (gdial);
-	rootg()->add (dshadowg);
 
-	rootg()->add (_tShadow2 = new SnTransform);
-	rootg()->add (snm);
-	rootg()->add (_gHands);
+	GsArray<GsPnt> falconpnt;
+	//falconpnt.push() = GsPnt(250.f, 250.f, 0.0f);
+	//falconpnt.push() = GsPnt(-250.f, 250.f, 0.0f);
+	//falconpnt.push() = GsPnt(-250.f, 250.f, 250.0f);
+	//falconpnt.push() = GsPnt(250.f, 250.f, -250.0f);
+
+	float r = 500.f;
+	for (float theta = 0.0f; theta < gs2pi; theta += gs2pi /12.0f) {
+		falconpnt.push() = GsPnt(r*cosf(theta), 250.f, r*sinf(theta));
+	}
+
+	showMyPoints(falconpnt); // shows the points created.
+	float deltat = 0.001f;
+	for (int i = 1; i < falconpnt.size() - 2; i ++) {
+		for(float t = 0.0f; t < 1.0f ; t+= deltat) {
+			GsPnt P = CRSplines(t, i, falconpnt); // use CR splines
+			falconPath.push() = P; // Creates the path
+		}
+	}
+
 }
 
+void MyViewer::showMyPoints(GsArray<GsPnt> P) {
+	SnModel* sPoints;
+	for (int i = 0; i < P.size(); i++) {
+		sPoints = new SnModel;
+		sPoints->model()->make_sphere(P[i], 20.0f, 50, true);
+		sPoints->color(GsColor::red);
+		rootg()->add(sPoints);
+	}
+}
+
+// Below is an example of how to control the main loop of an animation:
 void MyViewer::run_animation ()
 {
-	//if ( _animating ) return; // prevent recursive calls
+	//if ( _animating ) return; // avoid recursive calls
 	//_animating = true;
-
-	//// access the transformations to use
-	//// (pointers below could have been saved during scene construction for easier access)
-	//SnTransform* minT = _gHands->get<SnGroup>(0)->get<SnTransform>(0);
-	//SnTransform* secT = _gHands->get<SnGroup>(1)->get<SnTransform>(0);
-
-	//double frdt = 1.0/60.0; // delta time to reach given number of frames per second
-	//double t=0, lt=0, t0=gs_time();
-
-	//float degreeInc = -1.0f*(float)GS_PI/30.0f;
 	//
+	//int ind = gs_random ( 0, rootg()->size()-1 ); // pick one child
+	//SnManipulator* manip = rootg()->get<SnManipulator>(ind); // access one of the manipulators
+	//GsMat m = manip->mat();
+
+	//double frdt = 1.0/30.0; // delta time to reach given number of frames per second
+	//double v = 4; // target velocity is 1 unit per second
+	//double t=0, lt=0, t0=gs_time();
 	//do // run for a while:
 	//{	while ( t-lt<frdt ) { ws_check(); t=gs_time()-t0; } // wait until it is time for next frame
+	//	double yinc = (t-lt)*v;
+	//	if ( t>2 ) yinc=-yinc; // after 2 secs: go down
 	//	lt = t;
-
-	//	totSecDegrees += degreeInc; // note the increment is negative
-	//	if (totSecDegrees + GS_2PI <= 0)
-	//	{	totSecDegrees = 0.0f;
-	//		totMinDegrees += degreeInc;
-	//		if (totMinDegrees + GS_2PI <= 0)
-	//		{	totMinDegrees = 0.0f;
-	//		}
-	//	}
-
-	//	secT->get().rotz(totSecDegrees);
-	//	minT->get().rotz(totMinDegrees);
-
+	//	m.e24 += (float)yinc;
+	//	if ( m.e24<0 ) m.e24=0; // make sure it does not go below 0
+	//	manip->initial_mat ( m );
 	//	render(); // notify it needs redraw
 	//	ws_check(); // redraw now
-
-	//} while ( _animating );
+	//}	while ( m.e24>0 );
 	//_animating = false;
 
+	double frdt = 1.0 / 60.0;
+	double t = 0, lt = 0, t0 = gs_time();
+	int i = 0;
+	do {
+		while (t - lt < frdt) { ws_check(); t = gs_time() - t0; }
+		if (i < falconPath.size()) {
+			tFalcon->get().translation(falconPath[i]);
+		}
+		else {
+			i = 0;
+		}
+		i++;
+		lt = t;
+		render(); // notify it needs redraw
+		ws_check(); // redraw now
+	} while (true);
+	
 
-	double frdt = 1.0 / 30.0;
-	double t, lt, t0 = gs_time();
-	gtime = 0.0f;
-	/*if (camView == true) {
-		camera().eye.x = 10;
-		camera().eye.y = 280;
-		camera().eye.z = 10;
-		render();
-		ws_check();
-	}
-	else {*/
-		t = 0;
-		lt = 0;
-		do {
-			lt = gs_time() - t0;
-			if (lt < 2.0f) {
-				camera().eye.x += 0.001f;
-				camera().eye.z += 0.001f;
-				camera().center.y = 0.0f;
-			}
-			if (lt > 2.0f && lt <= 5.0f) {
-				camera().eye.x -= 0.0010f;
-				camera().eye.z -= 0.0010f;
-			}
-			if (lt > 5.0f && lt <= 10.0f) {
-				camera().eye.x -= 0.0020f;
-				camera().eye.z -= 0.0020f;
-			}
-			if (lt > 10.0f && lt <= 15.0f) {
-				//camera().eye.x -= 0.0010f;
-				camera().eye.z -= 0.0020f;
-				camera().eye.y += 0.0010f;
-			}
-			render();
-			ws_check();
-			message().setf("local time = % f", lt);
-		} while (lt <= 15.0f);
-	//}
 }
 
-void MyViewer::update_shadow ()
-{
-	float R = dialR+lightPos.y;
-	GsMat T1, T2;
-	T1.setrans(lightPos*-1.0f);
-	T2.setrans(lightPos);
-
-	GsMat shadowTdial(
-		-R, 0, 0, 0,
-		0, -R, 0, 0,
-		0, 0, -R, 0,
-		0, 1, 0, 0);
-	_tShadow1->get() = T2*shadowTdial*T1;
-
-	R-=0.001f;
-	GsMat shadowT(
-		-R, 0, 0, 0,
-		0, -R, 0, 0,
-		0, 0, -R, 0,
-		0, 1, 0, 0);
-	_tShadow2->get() = T2*shadowT*T1;
-
-	render();
-}
-
-void MyViewer::show_normals ( bool b )
+void MyViewer::show_normals ( bool view )
 {
 	// Note that primitives are only converted to meshes in GsModel
-	// at the first render call.
+	// at the first draw call.
 	GsArray<GsVec> fn;
-	for ( int k=0; k<_gHands->size(); k++ )
-	{	SnGroup* g = _gHands->get<SnGroup>(k);
-		SnShape* s = g->get<SnShape>(1);
-		SnLines* l = g->get<SnLines>(2);
-		if ( !b ) { l->visible(false); continue; }
+	SnGroup* r = (SnGroup*)root();
+	for ( int k=0; k<r->size(); k++ )
+	{	SnManipulator* manip = r->get<SnManipulator>(k);
+		SnShape* s = manip->child<SnGroup>()->get<SnShape>(0);
+		SnLines* l = manip->child<SnGroup>()->get<SnLines>(1);
+		if ( !view ) { l->visible(false); continue; }
 		l->visible ( true );
 		if ( !l->empty() ) continue; // build only once
 		l->init();
@@ -242,51 +251,24 @@ int MyViewer::handle_keyboard ( const GsEvent &e )
 {
 	int ret = WsViewer::handle_keyboard ( e ); // 1st let system check events
 	if ( ret ) return ret;
-	bool update=false;
+
+	double frdt = 1.0 / 30.0;
+	double t, lt, t0 = gs_time();
+	gtime = 0.0f;
+	t = 0;
+	lt = 0;
 
 	switch ( e.key )
-	{	case GsEvent::KeyEsc: gs_exit(); break;
-		//case GsEvent::KeySpace: if(_animating)_animating=false; else run_animation(); break;
-		case GsEvent::KeySpace: {
-			run_animation();
-			//camView = !camView;
-			return 1;
-		}
-		case GsEvent::KeyEnter:
-			totSecDegrees = totMinDegrees = 0;
-			update = true;
-			break;
-		case 'q':
-			lightPos += GsVec(0.1f, 0, 0);
-			update = true;
-			break;
-		case 'a':
-			lightPos -= GsVec(0.1f, 0, 0);
-			update = true;
-			break;
-		case 'w':
-			lightPos += GsVec(0, 0.1f, 0);
-			update = true;
-			break;
-		case 's':
-			lightPos -= GsVec(0, 0.1f, 0);
-			update = true;
-			break;
-		case 'e':
-			lightPos += GsVec(0, 0, 0.1f);
-			update = true;
-			break;
-		case 'd':
-			lightPos -= GsVec(0, 0, 0.1f);
-			update = true;
-			break;
+	{	case GsEvent::KeyEsc : gs_exit(); return 1;
 		case 'n' : { bool b=!_nbut->value(); _nbut->value(b); show_normals(b); return 1; }
+		case ' ': {
+			camera().eye.x += 10.0f;
+			camera().center;
+			ws_check();
+			render();
+			break;
+		}
 		default: gsout<<"Key pressed: "<<e.key<<gsnl;
-	}
-
-	if ( update )
-	{	_gLight->get<SnTransform>(0)->get().setrans(lightPos);
-		update_shadow ();
 	}
 
 	return 0;
@@ -296,7 +278,7 @@ int MyViewer::uievent ( int e )
 {
 	switch ( e )
 	{	case EvNormals: show_normals(_nbut->value()); return 1;
-		case EvAnimate: if(_animating)_animating=false; else run_animation(); return 1;
+		case EvAnimate: run_animation(); return 1;
 		case EvExit: gs_exit();
 	}
 	return WsViewer::uievent(e);
